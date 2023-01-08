43547a755ccbd6475ec6c3e85b8ee5373e203cce
Helena Man <helena.man@itv.com>
2023-01-08 13:40:01 +0000

    Added about page

--- django/pages/urls.py
+++ django/pages/urls.py
@@ -1,12 +1,13 @@
 # pages/urls.py
 from django.urls import path
 #from .views import homePageView
-from .views import HomePageView
+from .views import HomePageView, AboutPageView
 
 # urlpatterns = [
 #    path("", homePageView, name="home"),
 #]
 
 urlpatterns = [
-    path("", HomePageView.as_view(), name="home"),
+    path("home.html", HomePageView.as_view(), name="home"),
+    path("about/", AboutPageView.as_view(), name="about")
 ]

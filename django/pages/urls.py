39452c40882236d3d7c05651da2fc748abd5a8b7
Helena Man <manhelena@yahoo.co.uk>
2023-03-26 20:17:10 +0100

    It works now

--- django/pages/urls.py
+++ django/pages/urls.py
@@ -8,6 +8,6 @@ from .views import HomePageView, AboutPageView
 #]
 
 urlpatterns = [
-    path("home.html", HomePageView.as_view(), name="home"),
+    path("", HomePageView.as_view(), name="home"),
     path("about/", AboutPageView.as_view(), name="about")
 ]
--- django/pages/views.py
+++ django/pages/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 
-# def homePageView(request):
-#     return HttpResponse("Hello, World!")
+#def homePageView(request):
+#    return HttpResponse("Hello, World!")
 
 class HomePageView(TemplateView):
     template_name = "home.html"
